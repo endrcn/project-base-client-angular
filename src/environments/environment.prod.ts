c4567b73b6658f2e840cee415369780afbf43a5d
endrcnn <endercan34@gmail.com>
2023-01-28 00:54:01 +0300

    angular updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  url: "http://localhost:3000"
+  url: "http://localhost:3000/"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  url: "http://localhost:3000"
+  url: "http://localhost:3000/"
 };
 
 /*
