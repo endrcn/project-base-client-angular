c9583be2dd82e768f657a553676a21995f8df181
endrcnn <endercan34@gmail.com>
2023-02-01 01:37:06 +0300

    functionality done

--- src/app/users/user-add/user-add.component.ts
+++ src/app/users/user-add/user-add.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { lastValueFrom } from 'rxjs';
+import { Alert } from 'src/app/lib/alert';
+import { UsersService } from '../services/users.service';
+import { UserListComponent } from '../user-list/user-list.component';
 
 @Component({
   selector: 'app-user-add',
@@ -8,11 +13,32 @@ import { Component, OnInit } from '@angular/core';
 export class UserAddComponent implements OnInit {
 
   user: any = {};
-  roles: any = [];
+  @Input() roles: any;
+  @Input() parent!: UserListComponent;
 
-  constructor() { }
+  constructor(private usersService: UsersService, private alert: Alert, private translate: TranslateService) { }
 
   ngOnInit(): void {
+  }
+
+  async add() {
+    let resp;
+    try {
+      resp = await lastValueFrom(this.usersService.add(this.user));
+
+      this.parent.get();
+
+      this.alert.success(this.translate.instant("COMMON.SUCCESS_TITLE"), this.translate.instant("COMMON.SUCCESS_ADD"))
+
+      this.user = {};
+
+      const btnAddModalClose = document.getElementById("addModalClose")!
+
+      btnAddModalClose.click();
+
+    } catch (err: any) {
+      this.alert.errorHandler(err, true);
+    }
   }
 
 }
