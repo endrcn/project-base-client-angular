fee1c6c474abc099dc2a9ad157e0195cbc4326a4
endrcnn <endercan34@gmail.com>
2023-02-01 01:28:31 +0300

    form inputs have been added

--- src/app/users/user-add/user-add.component.ts
+++ src/app/users/user-add/user-add.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserAddComponent implements OnInit {
 
+  user: any = {};
+  roles: any = [];
+
   constructor() { }
 
   ngOnInit(): void {
