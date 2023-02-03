8b42aaaf4b225a765f3872c2d0960331d0abdf6b
endrcnn <endercan34@gmail.com>
2023-02-04 00:49:19 +0300

    role add component has done

--- src/app/roles/role-add/role-add.component.ts
+++ src/app/roles/role-add/role-add.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { lastValueFrom } from 'rxjs';
+import { Alert } from 'src/app/lib/alert';
+import { RoleListComponent } from '../role-list/role-list.component';
+import { RolesService } from '../services/roles.service';
 
 @Component({
   selector: 'app-role-add',
@@ -7,9 +12,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RoleAddComponent implements OnInit {
 
-  constructor() { }
+  role: any = {};
+  @Input() parent!: RoleListComponent;
+  @Input() privileges: any = [];
+  @Input() privilegeGroups: any = [];
+  permissions: any = {};
+
+  constructor(private rolesService: RolesService, private alert: Alert, private translate: TranslateService) { }
 
   ngOnInit(): void {
+  }
+
+  async add() {
+    try {
+
+      this.role.permissions = [];
+
+      for (let key of Object.keys(this.permissions)) {
+        if (this.permissions[key])
+          this.role.permissions.push(key);
+      }
+
+      await lastValueFrom(this.rolesService.add(this.role));
+
+      this.parent.get();
+
+      this.alert.success(this.translate.instant("COMMON.SUCCESS_TITLE"), this.translate.instant("COMMON.SUCCESS_ADD"))
+
+      const btnAddModalClose = document.getElementById("addRoleModalClose")!
+
+      btnAddModalClose.click();
+
+      this.role = {};
+
+    } catch (err: any) {
+      this.alert.errorHandler(err, true);
+    }
+  }
+
+  permissionChange(event: any) {
+    console.log("permission", this.permissions);
+    console.log(event.currentTarget.checked);
+
+  }
+
+  selectAll(event: any) {
+    if (event.currentTarget.checked) {
+      for (let i = 0; i < this.privileges.length; i++) {
+        let key = this.privileges[i].Key;
+        this.permissions[key] = true;
+      }
+    } else {
+      this.permissions = {};
+    }
   }
 
 }
