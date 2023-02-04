576e334151ef0c166710f151f3ff13f4592a98f0
endrcnn <endercan34@gmail.com>
2023-02-05 00:02:23 +0300

    user name & user menu has fixed

--- src/app/template/header/header.component.ts
+++ src/app/template/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  user: any = {};
+  subscription: any;
+
+  constructor(private auth: AuthService) { }
 
   ngOnInit(): void {
+    this.subscription = this.auth.getUser().subscribe(data => {
+      this.user = data;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  logout() {
+
+    this.auth.logout();
+
+  }
+
+  getUserRoles() {
+    if (this.user.roles) {
+      return this.user.roles.map((x: any) => `${x.role_name}`);
+    } else {
+      return "";
+    }
+  }
+
+  getCapitalLetters(text: string) {
+    let words = text.split(" ");
+    let letters = words.map((x: any) => x[0].toUpperCase())
+    return letters.join("");
   }
 
 }
